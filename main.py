ca5210e655456f0572ffea3057fb54d32ed95c2b
Oleg <oleg-sokol.sokolov2016@yandex.ru>
2024-04-03 22:08:33 +0500

    Test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def  text_rec(file_path):
     return 
@@ -6,10 +7,18 @@ def  text_rec(file_path):
    
 
 def main():
-    img = cv2.imread('img2.jpg', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('img2',img)
+    img = cv2.imread('images/img2.jpg', cv2.IMREAD_GRAYSCALE)
+    img = cv2.Canny(img, 120, 120)
+
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.bitwise_and(img,)
+    cv2.imshow('img',img)
     cv2.waitKey(0)
-    cv2.imwrite('img2_gray.jpg', img)
+    cv2.imwrite('images/img2_gray.jpg', img)
+
+
 
 if __name__ == "__main__":
     main()
